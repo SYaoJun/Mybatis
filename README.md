95856db54d6cb9cfe05469acb313db059777e75d
SYaoJun <940334249@qq.com>
2020-11-06 11:24:27 +0800

    add first mybatis program

---
+++ README.md
@@ -0,0 +1,3 @@
+## mybatis
+- mybatis-config.xml
+- userMapper.xml
---
+++ mybatis-01/src/main/java/com/bytedance/dao/UserDao.java
@@ -0,0 +1,9 @@
+package com.bytedance.dao;
+
+import com.bytedance.pojo.User;
+
+import java.util.List;
+
+public interface UserDao {
+    List<User> getUserList();
+}
